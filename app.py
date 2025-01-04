570cf32796764a8c5c20a6dc146da35ad1a12049
NimblePeeledWolf <158261531+NimblePeeledWolf@users.noreply.github.com>
2025-01-04 03:57:15 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 import pickle
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from ucimlrepo import fetch_ucirepo
 
